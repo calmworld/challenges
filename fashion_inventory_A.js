953380371ed03aacfb92f1929d4412fbc6e98562
Sarah <sarah.alhuriz@hotmail.com>
2021-03-05 12:28:34 -0700

    test passed

--- fashion_inventory_A.js
+++ fashion_inventory_A.js
@@ -68,6 +68,8 @@ function renderInventory(inventory) {
     return flatList;
 }
 
+
+
 // ASSERTION FUNCTION(S) TO BE USED
 function assertEqual(actual, expected, testName) {
     if (actual === expected) {
@@ -79,23 +81,25 @@ function assertEqual(actual, expected, testName) {
   
 // TESTS FOR RENDER INVENTORY
 var inventory = [
-{
-    name: 'Brunello Cucinelli',
-    shoes: [
-    {name: 'tasselled black low-top lace-up', price: 1000},
-    {name: 'tasselled green low-top lace-up', price: 1100},
-    {name: 'plain beige suede moccasin', price: 950},
-    {name: 'plain olive suede moccasin', price: 1050}
-    ]
-},
-{
-    name: 'Gucci',
-    shoes: [
-    {name: 'red leather laced sneakers', price: 800},
-    {name: 'black leather laced sneakers', price: 900}
-    ]
-}
+    {
+        name: 'Brunello Cucinelli',
+        shoes: [
+        {name: 'tasselled black low-top lace-up', price: 1000},
+        {name: 'tasselled green low-top lace-up', price: 1100},
+        {name: 'plain beige suede moccasin', price: 950},
+        {name: 'plain olive suede moccasin', price: 1050}
+        ]
+    },
+    {
+        name: 'Gucci',
+        shoes: [
+        {name: 'red leather laced sneakers', price: 800},
+        {name: 'black leather laced sneakers', price: 900}
+        ]
+    }
 ];
+
+// console.log(renderInventory(inventory));
 
 var actualFlatList = renderInventory(inventory);
 var expectedFlatList= "Brunello Cucinelli, tasselled black low-top lace-up, 1000\nBrunello Cucinelli, tasselled green low-top lace-up, 1100\nBrunello Cucinelli, plain beige suede moccasin, 950\nBrunello Cucinelli, plain olive suede moccasin, 1050\nGucci, red leather laced sneakers, 800\nGucci, black leather laced sneakers, 900";
