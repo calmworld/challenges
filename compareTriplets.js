4a08316db9aa6bc9f1db3cc1be9e1a3a8961ed90
Sarah <sarah.alhuriz@hotmail.com>
2021-03-17 14:32:22 -0600

    compareTriplet

---
+++ compareTriplets.js
@@ -0,0 +1,76 @@
+/*
+Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+
+The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+
+The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+
+If a[i] > b[i], then Alice is awarded 1 point.
+If a[i] < b[i], then Bob is awarded 1 point.
+If a[i] = b[i], then neither person receives a point.
+Comparison points is the total points a person earned.
+
+Given a and b, determine their respective comparison points.
+
+Example
+
+a = [1, 2, 3]
+b = [3, 2, 1]
+For elements *0*, Bob is awarded a point because a[0] .
+For the equal elements a[1] and b[1], no points are earned.
+Finally, for elements 2, a[2] > b[2] so Alice receives a point.
+The return array is [1, 1] with Alice's score first and Bob's second.
+
+Function Description
+
+Complete the function compareTriplets in the editor below.
+
+compareTriplets has the following parameter(s):
+
+int a[3]: Alice's challenge rating
+int b[3]: Bob's challenge rating
+Return
+
+int[2]: Alice's score is in the first position, and Bob's score is in the second.
+Input Format
+
+The first line contains 3 space-separated integers, a[0], a[1], and a[2], the respective values in triplet a.
+The second line contains 3 space-separated integers, b[0], b[1], and b[2], the respective values in triplet b.
+*/
+
+/*
+In this example:
+
+Now, let's compare each individual score:
+
+, so Alice receives  point.
+, so nobody receives a point.
+, so Bob receives  point.
+Alice's comparison score is , and Bob's comparison score is . Thus, we return the array .
+*/
+
+function compareTriplets(a, b) {
+    const aliceArray = a;
+    const bobArray = b;
+    let aliceTotal = 0; 
+    let bobTotal = 0;
+    
+    // both arrays have length of 3 so aliceArray.length 
+    // or bobArray.length will both work
+    for (let i = 0; i < aliceArray.length; i++) {
+        if (aliceArray[i] > bobArray[i]) {
+            aliceTotal++; // alice gets a point
+        } else if (aliceArray[i] < bobArray[i]) {
+            bobTotal++; // bob gets a point
+        } else {
+            continue; // they both get zero points so skip
+        }
+    }
+    return [aliceTotal, bobTotal];
+}
+
+const a = [1, 2, 3]
+const b = [3, 2, 1]
+
+console.log(compareTriplets(a, b)); 
+//The return array is [1, 1] with Alice's score first and Bob's second
