18196cb7980ad5b315c12f41f5db7fd1756b5cb6
Sarah <sarah.alhuriz@hotmail.com>
2021-03-26 18:10:37 -0600

    list all values

---
+++ listAllValues.js
@@ -0,0 +1,32 @@
+/*
+Write a function called "listAllValues" which returns an array of all the input object's values.
+
+Note that the input may have a different number of keys and values than the given sample. E.g. it should also handle an input like:
+
+Do not use "Object.values" to solve this prompt.
+*/
+
+//input: object
+//output: array of input object values
+
+function listAllValues(obj) {
+  //create a values var, assign to empty array
+  var values = [];
+  //iterate over object using for-in loop
+  for (var key in obj) {
+    //push values with current obj[key]
+    values.push(obj[key]);
+  }
+  //return values
+  return values;
+}
+
+// testing cases //
+var output = listAllValues({
+  name: "Krysten",
+  age: 33,
+  hasPets: false,
+});
+console.log(output); //--> ['Krysten', 33, false]
+
+
