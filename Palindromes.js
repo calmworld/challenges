f0afae6b3d9c27e5fee0c4f24ad12a57f575d609
Sarah <sarah.alhuriz@hotmail.com>
2021-06-02 11:21:16 -0600

    is palindrome

--- Palindromes.js
+++ Palindromes.js
@@ -1,7 +1,7 @@
 function palindrome(str) {
-    if (str.length === "") {
-        return 
-    }
     let result = str.toLowerCase().replace(/[W_]/g, "");
     return result === [...result].reverse().join("");
 }
+
+console.log(palindrome('nursesrun'));
+console.log(palindrome('nurses run'));
